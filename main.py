d850c35b02603005a53325f3893dc0105449e59d
dohako <dohako@inbox.ru>
2021-02-07 18:19:40 +0300

    ADD added correct algorithm for SPB

--- main.py
+++ main.py
@@ -38,35 +38,79 @@ conn = psycopg2.connect(
 conn.autocommit = True
 cursor = conn.cursor()
 # TODO check if base is empty
-creating_base = True
-
-# if base is empty or just created - fill base
-if creating_base is True:
+try:
     if os.path.isfile('base.json') is False:
         loguru.logger.error('Добавьте файл .json с базой')
         quit()
     with open('base.json', 'r', encoding='UTF-8') as base:
         data = json.load(base)
         loguru.logger.info(len(data[0]))
-    # TODO make table creation
     my_keys = [i for i in data[0].keys()]
     sql = f'''
         CREATE TABLE TEST (
-        {my_keys[0]} int2, 
+        {my_keys[0]} int2 PRIMARY KEY, 
         {my_keys[1]} int2, 
         {my_keys[2]} varchar(255), 
         {my_keys[3]} int2
         )
         '''
     cursor.execute(sql)
-    try:
-        for id, parent_id, name, type in data:
-            loguru.logger.info(id)
-            sql = f'''
-                    INSERT INTO TEST VALUES (
-                    {id}, {parent_id}, {name}, {type}
-                    )
-                '''
+except psycopg2.Error as ex:
+    loguru.logger.info(ex)
+    loguru.logger.info("Table already exists")
+
+try:
+    for d in data:
+        if d[my_keys[1]] is None:
+            d[my_keys[1]] = 'DEFAULT'
+        sql = f'''
+                INSERT INTO TEST ({my_keys[0]}, {my_keys[1]}, {my_keys[2]}, {my_keys[3]}) VALUES (
+                {d[my_keys[0]]}, {d[my_keys[1]]}, '{d[my_keys[2]]}', {d[my_keys[3]]}
+                )
+            '''
+        cursor.execute(sql)
+except Exception as ex:
+    loguru.logger.error(ex)
+    loguru.logger.info("base is full")
+
+loguru.logger.info('Start process')
+while True:
+    input_id = input('Введите идентификатор\n')
+    if input_id.isdecimal() is False:
+        loguru.logger.info('Wrong input')
+        continue
+    sql = f'''SELECT * FROM TEST WHERE id = {int(input_id)}'''
+    cursor.execute(sql)
+    info_from_base = cursor.fetchone()
+    loguru.logger.info(info_from_base)
+    if info_from_base[1] is None:
+        loguru.logger.info(info_from_base[2])
+        continue
+    sql = f'''SELECT * FROM TEST WHERE id = {info_from_base[1]}'''
+    cursor.execute(sql)
+    second_info = cursor.fetchone()
+    loguru.logger.info(second_info)
+    if second_info[1] is None:
+        loguru.logger.info(f'{info_from_base[2]} в {second_info[2]}')
+        continue
+    sql = f'''SELECT * FROM TEST WHERE id = {second_info[1]}'''
+    cursor.execute(sql)
+    third_info = cursor.fetchone()
+    loguru.logger.info(third_info)
+    if third_info[1] is None:
+        loguru.logger.info(f'сотрудник {info_from_base[2]} в {second_info[2]} из {third_info[2]}')
+        sql = f'''SELECT id FROM TEST WHERE parentid = {third_info[0]}'''
+        cursor.execute(sql)
+        departments = cursor.fetchall()
+        loguru.logger.info(departments)
+        result = f'{third_info[2]}: '
+        for department in departments:
+            sql = f'''SELECT name FROM TEST WHERE parentid = {department[0]}'''
             cursor.execute(sql)
-    except Exception as ex:
-        loguru.logger.error(ex)
+            value = [value[0] for value in cursor.fetchall()]
+            for v in value:
+                result += f'{v}, '
+        result = result[:len(result)-2]
+        loguru.logger.info(result)
+        continue
+conn.close()

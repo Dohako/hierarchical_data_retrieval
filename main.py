c282db1cbcd469908c2e54d37c2151ce6cd2cf49
dohako <dohako@inbox.ru>
2021-02-07 13:07:05 +0300

    ADD added base creation and exception cather

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import os
 import loguru
+import psycopg2
+import dotenv
 
 
-def first():
-    pass
+dotenv.load_dotenv('./.env')
+DATABASE = os.getenv("DATABASE")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
 
+#establishing the connection
+conn = psycopg2.connect(
+   database=DATABASE, user=USER, password=PASSWORD, host=HOST, port= PORT
+)
+conn.autocommit = True
+cursor = conn.cursor()
+try:
+   sql = '''CREATE database mydb''';
+   cursor.execute(sql)
+   print("Database created successfully........")
+except psycopg2.Error:
+   loguru.logger.info("Base already exists")
 
-loguru.logger.add('log.log')
-
-if os.path.isfile('base.json') is False:
-    loguru.logger.info('Добавьте файл с базой')
-else:
-    loguru.logger.info('Добавьте файл с базой')
+#Closing the connection
+conn.close()
+# loguru.logger.add('log.log')
+#
+#
+#
+# if os.path.isfile('base.json') is False:
+#     loguru.logger.info('Добавьте файл с базой')
+# else:
+#     loguru.logger.info('Добавьте файл с базой')

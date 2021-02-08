76b6acf08341471d9ab87e991c907a99f80a93bf
dohako <dohako@inbox.ru>
2021-02-08 11:07:39 +0300

    FIN finished algorithm

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ conn = psycopg2.connect(
 )
 conn.autocommit = True
 cursor = conn.cursor()
-creating_base:bool = False
+creating_base: bool = False
 database_name: str = 'mydb'
 try:
     sql = '''CREATE database mydb'''
@@ -31,6 +31,7 @@ try:
 except psycopg2.Error as ex:
     loguru.logger.error(ex)
     loguru.logger.info("Base already exists")
+    print('База уже существует, желаете продолжить?\n')
 conn.close()
 conn = psycopg2.connect(
     database=database_name, user=USER, password=PASSWORD, host=HOST, port=PORT
@@ -99,6 +100,9 @@ while True:
     loguru.logger.info(third_info)
     if third_info[1] is None:
         loguru.logger.info(f'сотрудник {info_from_base[2]} в {second_info[2]} из {third_info[2]}')
+        if info_from_base[3] != 3:
+            loguru.logger.info(f'{info_from_base[2]} принадлежащий {second_info[2]} находящийся в{third_info[2]}')
+            continue
         sql = f'''SELECT id FROM TEST WHERE parentid = {third_info[0]}'''
         cursor.execute(sql)
         departments = cursor.fetchall()
@@ -110,7 +114,37 @@ while True:
             value = [value[0] for value in cursor.fetchall()]
             for v in value:
                 result += f'{v}, '
-        result = result[:len(result)-2]
+        result = result[:len(result) - 2]
         loguru.logger.info(result)
         continue
+    sql = f'''SELECT * FROM TEST WHERE id = {third_info[1]}'''
+    cursor.execute(sql)
+    fourth_info = cursor.fetchone()
+    loguru.logger.info(fourth_info)
+    if fourth_info[1] is not None:
+        loguru.logger.error(f'something wrong with id {info_from_base[0]} because id {fourth_info[0]}'
+                            f'have parentid {fourth_info[1]}')
+        continue
+    sql = f'''SELECT id FROM TEST WHERE parentid = {fourth_info[0]}'''
+    cursor.execute(sql)
+    departments = cursor.fetchall()
+    loguru.logger.info(departments)
+    result = f'{fourth_info[2]}: '
+    for department in departments:
+        sql = f'''SELECT id, name, type FROM TEST WHERE parentid = {department[0]}'''
+        cursor.execute(sql)
+        values = []
+        for value in cursor.fetchall():
+            if value[2] == 3:
+                values.append(value[1])
+            elif value[2] == 2:
+                sql = f'''SELECT name FROM TEST WHERE parentid = {value[0]}'''
+                cursor.execute(sql)
+                for v in cursor.fetchall():
+                    values.append(v[0])
+        # value = [value[0] for value in cursor.fetchall()]
+        for v in values:
+            result += f'{v}, '
+    result = result[:len(result) - 2]
+    loguru.logger.info(result)
 conn.close()
